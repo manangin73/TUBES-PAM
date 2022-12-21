19a76b1e8c421e04a100758b210208baeb637a25
Dodi Devrian Andrianto <84231458+DodiDevrian@users.noreply.github.com>
2022-12-21 11:44:56 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Navigation from './src/Navigation';
+
 export default function App() {
   return (
     <Navigation />
