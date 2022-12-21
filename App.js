666cb165bfe15c71265c0105ab574e8bac2e9d2e
GilangAsh14 <91508048+GilangAsh14@users.noreply.github.com>
2022-12-21 11:08:30 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,8 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Navigation from './src/Navigation';
+export default function App() {
+  return (
+    <Navigation />
+  );
+}
